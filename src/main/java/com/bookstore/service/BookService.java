447b475cc379f315d5e8d7f81311c3ea01750260
Ahmed Abdelmongy <ahmednagi318@gmail.com>
2025-04-04 22:29:18 +0200

    create orderStatus and Role enums and implement bookService class

---
+++ src/main/java/com/bookstore/service/BookService.java
@@ -0,0 +1,23 @@
+package com.bookstore.service;
+
+import com.bookstore.entity.Book;
+import com.bookstore.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public void save(Book book){
+        bookRepository.save(book);
+    }
+
+    public List<Book> getAllBooks(){
+        return bookRepository.findAll();
+    }
+}

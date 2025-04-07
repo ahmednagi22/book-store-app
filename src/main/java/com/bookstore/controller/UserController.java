4b040c3659ac2263326fcec334d94e0d264569d0
Ahmed Abdelmongy <ahmednagi318@gmail.com>
2025-04-07 07:29:18 +0200

    implement login.html and add login handler methods in userController

--- src/main/java/com/bookstore/controller/UserController.java
+++ src/main/java/com/bookstore/controller/UserController.java
@@ -1,17 +1,16 @@
 package com.bookstore.controller;
 
+import com.bookstore.dto.LoginDTO;
 import com.bookstore.dto.UserDTO;
-import com.bookstore.entity.User;
-import com.bookstore.service.BookService;
 import com.bookstore.service.UserService;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
@@ -29,6 +28,7 @@ public class UserController {
         model.addAttribute("userDto",new UserDTO());
         return "register";
     }
+
     //process registration
     @PostMapping("/register")
     public String registerUser(
@@ -51,13 +51,32 @@ public class UserController {
     }
 
     @GetMapping("/login")
-    public String loginForm(){
+    public String showLoginForm(@RequestParam(value = "error", required = false) String error,
+                                @RequestParam(value = "logout", required = false) String logout,
+                                @RequestParam(value = "registered", required = false) String registered,
+                                Model model){
+        model.addAttribute("loginDto", new LoginDTO());
+
+        if (error != null)   model.addAttribute("loginError", "Invalid credentials");
+        if (logout != null)  model.addAttribute("logoutMsg", "You have been logged out");
+        if (registered != null) model.addAttribute("regMsg", "Registration successful. Please log in.");
+
         return "login";
     }
 
     @PostMapping("/login")
-    public String login(){
+    public String loginUser(
+            @Valid @ModelAttribute("loginDto") LoginDTO loginDto,
+            BindingResult bindingResult,
+            Model model){
 
+        if(bindingResult.hasErrors()){
+            return "login";
+        }
+        if (!userService.authenticateUser(loginDto.getEmail(), loginDto.getPassword())) {
+            model.addAttribute("loginError", "Invalid email or password.");
+            return "login";
+        }
         return "redirect:/books";
     }
 }
--- src/main/java/com/bookstore/service/UserService.java
+++ src/main/java/com/bookstore/service/UserService.java
@@ -1,33 +1,56 @@
 package com.bookstore.service;
 
 import com.bookstore.dto.UserDTO;
-import com.bookstore.entity.Book;
+import com.bookstore.exception.UserNotFoundException;
+import org.mindrot.jbcrypt.BCrypt;
 import com.bookstore.entity.User;
 import com.bookstore.enums.Role;
 import com.bookstore.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
 
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+
+    }
     public void registerUser(UserDTO userDTO) throws RuntimeException{
         // Check if user already exists
         if(userRepository.findByEmail(userDTO.getEmail()).isPresent()){
-            throw new RuntimeException("Email already in use");
+            throw new RuntimeException("Email already in use!!!");
         }
 
         //create New User
         User user = new User();
         user.setFullName(userDTO.getFullName());
         user.setEmail(userDTO.getEmail());
-        user.setPassword(userDTO.getPassword());
+
+        // encode password using BcCrypt
+        String hashedPassword = BCrypt.hashpw(userDTO.getPassword(),BCrypt.gensalt());
+        user.setPassword(hashedPassword);
+
         user.setRole(Role.CUSTOMER);// default user is customer
 
         //save the user
         userRepository.save(user);
+    }
+
+    public User getUserByEmail(String email){
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UserNotFoundException("User not found"));
+    }
+    public boolean authenticateUser(String email, String password){
+        // get user by email
+        User user = getUserByEmail(email);
+        //user doesn't exist, return false
+        if(user == null){
+            return false;
+        }
+        // check user password with stored password
+        return BCrypt.checkpw(password,user.getPassword());
     }
 }
